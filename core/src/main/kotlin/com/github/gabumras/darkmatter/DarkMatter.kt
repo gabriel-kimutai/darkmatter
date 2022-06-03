d1c15d72111519ad492fc2196849491bc6643528
gabumras <gabrielkimutai997@gmail.com>
2022-06-03 13:09:27 +0300

    Initial commit

---
+++ core/src/main/kotlin/com/github/gabumras/darkmatter/DarkMatter.kt
@@ -0,0 +1,6 @@
+package com.github.gabumras.darkmatter
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class DarkMatter : ApplicationAdapter()
